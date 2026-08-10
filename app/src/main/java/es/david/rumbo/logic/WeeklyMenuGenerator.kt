91f84fa59b9bec81d4e9ebc802da1d7b622b995d
David Prieto <frandavid100@gmail.com>
2026-08-10 18:51:24 +0200

    Import dish portion nutrition

--- app/src/main/java/es/david/rumbo/logic/WeeklyMenuGenerator.kt
+++ app/src/main/java/es/david/rumbo/logic/WeeklyMenuGenerator.kt
@@ -14,6 +14,7 @@ import es.david.rumbo.model.PlanningSlot
 import es.david.rumbo.model.Recommendation
 import es.david.rumbo.model.WeekDay
 import es.david.rumbo.model.nutrition
+import es.david.rumbo.model.nutritionForGrams
 import es.david.rumbo.model.resolvedGrams
 import java.util.Random
 import kotlin.math.abs
